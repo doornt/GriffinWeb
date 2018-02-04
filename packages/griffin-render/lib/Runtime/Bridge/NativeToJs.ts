e7168b9a443d51b2007579629f6c0a6c19bf2931
Sampson <dujianwxfdt@live.cn>
2018-02-04 12:05:03 +0800

    native

--- packages/griffin-render/lib/Runtime/Bridge/NativeToJs.ts
+++ packages/griffin-render/lib/Runtime/Bridge/NativeToJs.ts
@@ -3,19 +3,23 @@ import { Application } from "../Application/Application";
 import {ComponentManager} from "../../Manager/ComponentManager"
 
 
+declare var global:any;
+
 export class NativeToJs {
     public static init() {
-        (<any>global).dispatchEventToJs = (rootViewId: string, event: NativeEvent) => {
+        global.dispatchEventToJs = (rootViewId: string, event: NativeEvent) => {
             console.log(JSON.stringify(event))
             Application.instance.handleEventFromNative(rootViewId, event)
         }
 
-        (<any>global).registerNativeComponent = (tagName:string) =>{
+        global.registerNativeComponent = (tagName:string) =>{
             ComponentManager.instance.registerNativeView(tagName)
         }
 
-        (<any>global).registerModule = (ctr,name:String)=>{
-            console.log(ctr,name)
+        global.registerModule = (ctr,name:string)=>{
+            global[name] = ctr
+
+            console.log('register module',name)
         }
 
     }
